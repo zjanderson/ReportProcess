e529d3018ee255b3b78254348ebc99193b14ed9d
E.J. Bryant <ejbryant28@gmail.com>
2024-11-19 22:34:32 -0500

    cleaned up imports

--- Scripts/delete_emails.py
+++ Scripts/delete_emails.py
@@ -1,7 +1,6 @@
 import win32com.client
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
