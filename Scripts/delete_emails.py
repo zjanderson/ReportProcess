9804fd449e73580b841a9e78e8a1b9fb86ac156b
E.J. Bryant <ejbryant28@gmail.com>
2024-11-19 21:35:21 -0500

    Added more always delete folders

--- Scripts/delete_emails.py
+++ Scripts/delete_emails.py
@@ -3,13 +3,31 @@ import win32com.client
 def delete_emails_from_folder(folder_name):
     outlook = win32com.client.Dispatch("Outlook.Application")
     namespace = outlook.GetNamespace("MAPI")
-    inbox = namespace.Folders.Item("Your Email Account")
+    inbox = namespace.Folders.Item("zanderson@armada.net")
     folder = inbox.Folders.Item(folder_name)
    
     for item in list(folder.Items):
         item.Delete()
 
-DELETE_FOLDERS = ["Deleted Items"]
+DELETE_FOLDERS = [
+    "Deleted Items", 
+    "Weather Updates", 
+    "QCD", 
+    "Perishables", 
+    "McLane CFA", 
+    "MB CFA", 
+    "FA CFA", 
+    "CFA PR", 
+    "CFA Hubs", 
+    "CFA Hormel", 
+    "CFA Contingency", 
+    "CFA Hawaii", 
+    "CFA Canada", 
+    "2.0 CFA", 
+    "Fresh Beef", 
+    "Coverage", 
+    "BluePrism"
+    ]
 
 def execute_deletes():
     for folder in DELETE_FOLDERS:
